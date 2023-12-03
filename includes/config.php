45a688b4d2a33d3d7a32583a5db7196a63e0bf8d
ASHUTOSH <bds.ashu@gmail.com>
2023-12-03 16:46:12 +0530

    ffff

--- includes/config.php
+++ includes/config.php
@@ -6,8 +6,8 @@ define('DB_PASSWORD', '');
 define('DB_NAME', 'SafeDocHub');
 
 
-define('AWS_ACCESS_KEY', 'AKIA4IXFV3BRZ54TL766');
-define('AWS_SECRET_KEY', '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');
+define('AWS_ACCESS_KEY', '');
+define('AWS_SECRET_KEY', '');
 define('AWS_REGION', 'ap-south-1');
 define('AWS_BUCKET', 're-cvs');
 
