9cba54f551578cd815780be722ec72ecc894d1f8
ASHUTOSH <bds.ashu@gmail.com>
2023-12-03 16:42:54 +0530

    final

---
+++ README.md
@@ -0,0 +1,41 @@
+# SafeDocHub
+
+SafeDocHub is a secure document management application built with PHP, MySQL, and Amazon S3.
+
+## Table of Contents
+
+- [Features](#features)
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+  - [Clone the Repository](#1-clone-the-repository)
+  - [Install Dependencies](#2-install-dependencies)
+  - [Configure the Database](#3-configure-the-database)
+  - [Configure AWS Credentials](#4-configure-aws-credentials)
+  - [Run the Application](#5-run-the-application)
+- [Usage](#usage)
+- [Troubleshooting](#troubleshooting)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- **User Authentication:** Secure registration and login system.
+- **Document Upload:** Upload documents securely to Amazon S3.
+- **Dashboard:** View, manage, and organize your uploaded documents.
+
+## Prerequisites
+
+Before you begin, ensure you have the following installed:
+
+- [PHP](https://www.php.net/) (version >= 7.0)
+- [MySQL](https://www.mysql.com/)
+- [Composer](https://getcomposer.org/) for PHP package management
+- An [AWS S3](https://aws.amazon.com/s3/) account with access key and secret key
+
+## Installation
+
+### 1. Clone the Repository
+
+```bash
+git clone https://github.com/yourusername/safedochub.git
+```
---
+++ safedochub.sql
@@ -0,0 +1,114 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 03, 2023 at 12:12 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `safedochub`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `documents`
+--
+
+CREATE TABLE `documents` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `file_key` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `documents`
+--
+
+INSERT INTO `documents` (`id`, `user_id`, `name`, `file_key`) VALUES
+(1, 1, 'test', 'documents/656c5439818e7/Placement-test_job.pdf'),
+(4, 1, 'temp', 'documents/656c5994baa8e/Placement-RJ (1).pdf'),
+(5, 1, 'temp', 'documents/656c59bf1adbd/Placement-RJ (1).pdf'),
+(10, 2, 'ffff', 'documents/656c5eef7f430/Placement-test_job.pdf'),
+(11, 2, 'test', 'documents/656c5fe2d534a/Placement-test_job.pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'ashu', '$2y$10$F3cJNAlDSBYZyFraEFboeuQB4909iggLwQQ.awDob7xGePYuycCF2'),
+(2, 'ashuu', '$2y$10$ZoE0N2bACxpGt40fpKHfDOM3nvMbbv37/OcX5CH2PgeypfDTXgat6');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `documents`
+--
+ALTER TABLE `documents`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `documents`
+--
+ALTER TABLE `documents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `documents`
+--
+ALTER TABLE `documents`
+  ADD CONSTRAINT `documents_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
